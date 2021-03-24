5165b73d5a715537f67eb9cdbb8a6ad8be07ab38
aanderson3 <66184238+aanderson3@users.noreply.github.com>
2021-03-24 00:36:22 -0600

    cart widget

---
+++ Bookstore413/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bookstore413.Models;
+
+namespace Bookstore413.Components
+{
+    //view component to add widget for cart
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
